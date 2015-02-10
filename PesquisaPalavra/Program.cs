dcf706e7b1826bad2083fc6f70ea83d065e01da3
aranhasete <aranhajba@gmail.com>
2015-02-09 23:08:49 -0200

    Meu Segundo Commit

--- PesquisaPalavra/Program.cs
+++ PesquisaPalavra/Program.cs
@@ -22,9 +22,19 @@ namespace PesquisaPalavra
                 command = Console.ReadLine();
                 int posicao = PesquisaPalavra(command);
 
-                Console.WriteLine("Palavra " + command.ToString() + " encontrada na posição: " + posicao.ToString());
-                Console.WriteLine();
-                Console.WriteLine("Gatilhos gastos: " + gatilho.ToString());
+                if (posicao != -1)
+                {
+                    Console.WriteLine("Palavra " + command.ToString() + " encontrada na posição: " + posicao.ToString());
+                    Console.WriteLine();
+                    Console.WriteLine("Gatilhos gastos: " + gatilho.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Palavra " + command.ToString() + " não existente no dicionário!");
+                }
+                
+
+
 
             } while (command != "exit");
 
@@ -32,17 +42,20 @@ namespace PesquisaPalavra
 
         private static int PesquisaPalavra(string word)
         {
-            string url = string.Format("http://teste.way2.com.br/dic/api/words/");
+            //string[] letras = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            string url = "http://teste.way2.com.br/dic/api/words/{0}";
             bool bNaoEncontrou = true;
             int linha = 0;
             WebClient cliente = new WebClient();
             cliente.Credentials = new NetworkCredential();
+            int min = 0;
+            int max = 1;
 
 
-            while (bNaoEncontrou)
+            while (bNaoEncontrou || min < max)
             {
                 string w = word;
-                string wl = cliente.DownloadString(string.Format("http://teste.way2.com.br/dic/api/words/{0}", linha.ToString()));
+                string wl = cliente.DownloadString(string.Format(url, linha.ToString()));
                 int tw = word.Length;
                 int twl = wl.Length;
 
@@ -70,6 +83,11 @@ namespace PesquisaPalavra
                     }
 
                     linha++;
+                }
+                else
+                {
+                    linha = -1;
+                    bNaoEncontrou = false;
                 }
             }
             return linha;
